6f3c79a2e91d1a0dd9764c3ef9b70a7adf7a1404
gzemlianyi <georgii.zemlianyi@improving.com>
2026-08-20 17:58:49 -0400

    fix: Improve multithreading in TaskStateChangeEventProcessor

--- src/main/java/io/debezium/connector/spanner/task/SynchronizationTaskContext.java
+++ src/main/java/io/debezium/connector/spanner/task/SynchronizationTaskContext.java
@@ -64,6 +64,8 @@ public class SynchronizationTaskContext {
 
     private final PartitionFactory partitionFactory;
 
+    private final PartitionOffsetProvider partitionOffsetProvider;
+
     private final LowWatermarkStampPublisher lowWatermarkStampPublisher;
 
     private final Runnable finishingHandler;
@@ -100,6 +102,8 @@ public class SynchronizationTaskContext {
         this.task = task;
 
         this.connectorConfig = connectorConfig;
+
+        this.partitionOffsetProvider = partitionOffsetProvider;
 
         this.errorHandler = errorHandler;
 
@@ -239,11 +243,19 @@ public class SynchronizationTaskContext {
             this.taskStateChangeEventProcessor.stopProcessing();
             LOGGER.info("Task {}, Shut down TaskStateChangeEventProcessor", this.taskSyncContextHolder.get().getTaskUid());
 
+            // Shut down the partition-scheduling executor only after the event processor is stopped
+            // so no new schedulePendingPartitionsAsync() calls can arrive after this point.
+            this.taskStateChangeEventHandler.shutdown();
+            LOGGER.info("Task {}, Shut down TaskStateChangeEventHandler partition scheduler", this.taskSyncContextHolder.get().getTaskUid());
+
             this.lowWatermarkCalculationJob.stop();
             LOGGER.info("Task {}, Shut down LowWatermarkCalculationJob", this.taskSyncContextHolder.get().getTaskUid());
 
             this.rebalanceHandler.destroy();
             LOGGER.info("Task {}, Shut down rebalance handler", this.taskSyncContextHolder.get().getTaskUid());
+
+            this.partitionOffsetProvider.shutdown();
+            LOGGER.info("Task {}, Shut down PartitionOffsetProvider", this.taskSyncContextHolder.get().getTaskUid());
 
         }
         catch (Exception ex) {
